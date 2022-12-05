3b7d8273b6c8fe53df47af761743e5ecc4d3af29
craig <craig.bennett@conversify.com.au>
2022-12-05 15:47:47 +1000

    work in progress

---
+++ src/views/About.vue
@@ -0,0 +1,22 @@
+ <template>
+    <div class="about pa-6"> THIS IS THE ABOUT PAGE
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Et libero dolore molestias culpa quisquam maxime aliquam voluptas! Earum, 
+            laboriosam! Non fugiat sunt aperiam eius praesentium eveniet veniam, assumenda consectetur a?
+        </p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Et libero dolore molestias culpa quisquam maxime aliquam voluptas! Earum, 
+            laboriosam! Non fugiat sunt aperiam eius praesentium eveniet veniam, assumenda consectetur a?
+        </p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Et libero dolore molestias culpa quisquam maxime aliquam voluptas! Earum, 
+            laboriosam! Non fugiat sunt aperiam eius praesentium eveniet veniam, assumenda consectetur a?
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'About'
+}
+</script>
